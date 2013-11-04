477952ae685312a23536f1abb4eecc91bd06eadd
Thomas Baumann <thomas_b88@hotmail.com>
2013-11-04 20:17:29 +0100

    update comment

--- apsi/Labs/src/lab1/StartPermutation.java
+++ apsi/Labs/src/lab1/StartPermutation.java
@@ -10,10 +10,18 @@ public class StartPermutation {
 	public static void main(String[] args) throws IOException {
 		int count = 5;
 		int countFound = 0;
+
+		// create the text generators for original and fake message
 		TextGenerator tg = new TextGenerator("rsc/Patterns.txt", true);
 		TextGenerator tgOrg = new TextGenerator("rsc/Patterns_Org.txt", true);
+
+		// hashmap for hash and text of the fake message
 		HashMap<Integer, String> hashesFake = new HashMap<Integer, String>();
+
+		// hashmap for hash and text of the original message
 		HashMap<Integer, String> hashesOrg = new HashMap<Integer, String>();
+
+		// hashmap for all found collisions with the hash and list of all textes with this hashe
 		HashMap<Integer, ArrayList<String>> hashesFound = new HashMap<Integer, ArrayList<String>>();
 		
 		DESHash hashFunction = new DESHash();
@@ -28,12 +36,14 @@ public class StartPermutation {
 				int hash2 = hashFunction.hash(text2);
 				hashesOrg.put(hash2, text2);
 			}
+
 			// Collision detection
 			Iterator<Integer> it = hashesFake.keySet().iterator();
 			while(it.hasNext()) {
 				int h = it.next();
-				if (hashesOrg.containsKey(h)) {
-					if (hashesFound.containsKey(h)) {
+				if (hashesOrg.containsKey(h)) { // a collision has been found
+					if (hashesFound.containsKey(h)) { // this hash value was already found
+						// add this message to an existing hash
 						ArrayList<String> al = hashesFound.get(h);
 						String t = hashesFake.get(h);
 						String t2 = hashesOrg.get(h);
@@ -45,7 +55,7 @@ public class StartPermutation {
 							al.add(t2);
 						}
 						hashesFound.put(h, al);
-					} else {
+					} else { // this hash wasn't found before, add new entry
 						countFound++;
 						ArrayList<String> al = new ArrayList<String>();
 						al.add(hashesOrg.get(h));
