4706fabd14a7713e899426360f7b2011b1b56b44
Thomas Baumann <thomas_b88@hotmail.com>
2013-11-04 20:10:44 +0100

    comment update

--- apsi/Labs/src/lab1/Start.java
+++ apsi/Labs/src/lab1/Start.java
@@ -12,15 +12,22 @@ public class Start {
 		
 		DESHash hashFunction = new DESHash();
 
+		// get the string of the original message
 		String textOriginal = readFile("rsc/original.txt");
+
+		// build the original hash
 		int hashOriginal = hashFunction.hash(textOriginal);
+
 		int hash;
 		String text;
+
+		// get texts and build the hash until the same hash has been found
 		do {
 			text = tg.getNextText();
 			hash = hashFunction.hash(text);
 		} while (hashOriginal != hash);
 
+		// print the result
 		System.out.println(hashOriginal);
 		System.out.println(textOriginal);
 		System.out.println();
@@ -29,7 +36,13 @@ public class Start {
 
 	}
 
+	/**
+	 * Returns the content of the specified file as one string.
+	 * 
+	 * @param fileName The file the open
+	 */
 	private static String readFile(String fileName) throws IOException {
+		// reads 
 		StringBuilder text = new StringBuilder();
 		BufferedReader reader = new BufferedReader(new FileReader(fileName));
 		String line = reader.readLine();
