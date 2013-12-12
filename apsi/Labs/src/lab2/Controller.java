c98bab088657abbd39b2d80f04dd8f15c9e195ae
EgemenKaba <Egemen>
2013-12-12 18:16:00 +0100

    added line for debugging purposes and left a note (TODO)

--- apsi/Labs/src/lab2/Controller.java
+++ apsi/Labs/src/lab2/Controller.java
@@ -67,6 +67,7 @@ public class Controller {
 				String username = name + counter;
 				c.setUsername(username);
 				password = Utility.generateRandomString(32);
+//				errors.add(password); // TODO debugging
 				c.setPassword(Utility.hashString(password));
 			} else {
 				errors.add("Benutzereingaben sind nicht korrekt.");
